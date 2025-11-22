61c8b410f10cbee47aa8aa439630419feb61b6df
Alexei Raspereza <rasp@mail.desy.de>
2025-11-22 09:55:05 +0100

    script preparing list of file for submitting to condor

---
+++ Fitter/MuTauFR/scripts/PrepareSubmit.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3
+
+from TauFW.Plotter.plot.utils import ensuredir
+import TauFW.Fitter.MuTauFR.utils as utils
+import TauFW.Fitter.MuTauFR.styles as styles
+import TauFW.Fitter.MuTauFR.analysisMuTauFR as analysis
+import os
+
+def SubmitJob(**kwargs):
+
+    era = kwargs.get('era','2024')
+    chan = kwargs.get('channel','mutau')
+    sample = kwargs.get('sample','Muon0_2024B')
+    start = kwargs.get('start',0)
+    period = kwargs.get('period',10000000)
+    prong = kwargs.get('prong',1)
+    wpVsMu = kwargs.get('wpVsMu','VLoose')
+    wpVsE = kwargs.get('wpVsE','VVLoose')
+    wpVsJet = kwargs.get('wpVsJet','Medium')
+    applySF = kwargs.get('applySF',False)
+
+    # executable
+    cmssw_base = os.getenv('CMSSW_BASE')
+    
+    suffix = utils.defineSuffix(chan,era,wpVsJet,wpVsMu,wpVsMu,prong,applySF)
+    foldername = utils.condorFolder + '/' + suffix
+    condor_folder = ensuredir(foldername)
+    basefilename = f'{sample}_{start}'
+    filename_exec = f'{condor_folder}/{basefilename}.sh'
+    f = open(filename_exec,'w')
+    f.write('#!/bin/tcsh\n')
+    f.write(f'cd {cmssw_base}/src\n')
+    f.write('setenv SCRAM_ARCH el9_amd64_gcc10\n')
+    f.write('#export SCRAM_ARCH=el9_amd64_gcc10\n')
+    f.write('cmsenv\n')
+    f.write('cd TauFW/Fitter/MuTauFR\n')
+    f.write('echo $PWD\n')
+    commandline = f'./scripts/RunSelectionMuTau.py --era {era} --channel {chan} --sample {sample} --start {start} --period {period} --wpVsJet {wpVsJet} --wpVsMu {wpVsMu} --wpVsE {wpVsE} --prong {prong}\n'
+    f.write(commandline)
+    os.system(f'chmod u+x {filename_exec}')
+    
+    # configuration 
+    initialdir = os.getenv('CMSSW_BASE')+'/src/TauFW/Fitter/MuTauFR'
+    filename_config = f'{condor_folder}/{basefilename}.submit'
+    f = open(filename_config,'w')
+    f.write('universe = vanilla\n')
+    f.write(f'executable = {condor_folder}/{basefilename}.sh\n')
+    f.write(f'output = {condor_folder}/{basefilename}.out\n')
+    f.write(f'error = {condor_folder}/{basefilename}.err\n')
+    f.write(f'log = {condor_folder}/{basefilename}.log\n')
+    f.write('notification = Never\n')
+    f.write(f'initialdir = {initialdir}\n')
+    f.write('+MaxRuntime = 50000\n')
+    f.write('+RequestRuntime = 50000\n')
+    f.write('MY.WantOS = el9\n')
+    f.write('queue')
+
+    command = 'chmod u+x %s'%(filename_config)
+    os.system(command)
+    command = 'condor_submit %s'%(filename_config)
+    return command
+
+if __name__ == "__main__":
+
+    styles.InitROOT()
+    styles.SetStyle()
+
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('-e', '--era', dest='era', default='2024', choices=['2024'])
+    parser.add_argument('-c','--channel',dest='channel',default='mutau',choices=['mutau','mumu'])
+    parser.add_argument('-wpVsJet','--wpVsJet', dest='wpVsJet', default='Medium', choices=['Loose','Medium','Tight','VTight'])
+    parser.add_argument('-wpVsMu','--wpVsMu', dest='wpVsMu', default='VLoose', choices=['VLoose','Loose','Medium','Tight'])
+    parser.add_argument('-wpVsE','--wpVsE', dest='wpVsE', default='VVLoose', choices=['VVLoose','Loose','Medium','Tight'])
+    parser.add_argument('-prong','--prong', dest='prong',type=int,default=1,choices=[0,1,3])
+    parser.add_argument('-period','--period',dest='period',type=int,default=10000000)
+    args = parser.parse_args()
+
+    era = args.era
+    channel = args.channel
+    wpVsJet = args.wpVsJet
+    wpVsMu = args.wpVsMu
+    wpVsE = args.wpVsE
+    prong = args.prong
+    period = args.period
+
+    submitfile = 'submit.bash'
+    f = open(submitfile,'w')
+    f.write('#!/bin/bash\n')
+    
+    for sample in utils.data_samples[era]:
+        sampleToProcess = analysis.sampleMuTauFR(era,channel,sample)
+        nentries = sampleToProcess.GetEntries()
+        nperiods = int(nentries/period)
+        print(f'{sample}  entries={nentries}  nperiods={nperiods}')
+        for iperiod in range(0,nperiods+1):
+            command = SubmitJob(era=era,
+                                channel=channel,
+                                sample=sample,
+                                wpVsJet=wpVsJet,
+                                wpVsMu=wpVsMu,
+                                wpVsE=wpVsE,
+                                prong=prong,
+                                start=iperiod,
+                                period=period)
+            f.write(command+'\n')
+        
+    for sample in utils.mc_samples[era]:
+        sampleToProcess = analysis.sampleMuTauFR(era,channel,sample)
+        nentries = sampleToProcess.GetEntries()
+        nperiods = int(nentries/period)
+        print(f'{sample}  entries={nentries}  nperiods={nperiods}')
+        for iperiod in range(0,nperiods+1):
+            command = SubmitJob(era=era,
+                                channel=channel,
+                                sample=sample,
+                                wpVsJet=wpVsJet,
+                                wpVsMu=wpVsMu,
+                                wpVsE=wpVsE,
+                                prong=prong,
+                                start=iperiod,
+                                period=period)
+            f.write(command+'\n')
+    os.system('chmod u+x submit.bash')
